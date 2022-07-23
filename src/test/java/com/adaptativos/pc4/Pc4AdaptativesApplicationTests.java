bcaa9baff7ba5e80add53745d6ffcea4e4696d7c
Valentin2001UNI <Valentin2001UNI>
2022-07-23 18:41:48 -0500

    GO TEST

--- src/test/java/com/adaptativos/pc4/Pc4AdaptativesApplicationTests.java
+++ src/test/java/com/adaptativos/pc4/Pc4AdaptativesApplicationTests.java
@@ -1,13 +1,50 @@
 package com.adaptativos.pc4;
 
+import com.adaptativos.pc4.domain.Estudiante;
+import com.adaptativos.pc4.service.EstudianteServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class Pc4AdaptativesApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void test1(){
+
+        Estudiante estudiante = new Estudiante("Roberto", "2005205R", "roberto@abierto.com");
+
+        Assertions.assertEquals(estudiante.getName(),"Roberto");
+
+
+    }
+
+    @Test
+    public void test2(){
+
+        EstudianteServiceImpl estudianteService = new EstudianteServiceImpl();
+
+        final List<Estudiante> list = estudianteService.listarEstudiantes();
+
+        Assertions.assertEquals(((Object) list).getClass().getSimpleName(), ArrayList.class.getSimpleName());
+
+    }
+
+    @Test
+    public void test3(){
+
+        Estudiante estudiante = new Estudiante();
+
+        Assertions.assertNotEquals(estudiante.getName(), "Roberto");
+
+
     }
 
 }
